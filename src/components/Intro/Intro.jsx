b52485032253320bc068470afa1ad19fc6e9e9a6
GURU-VISHNU-SAI <131885828+vishnupenuvarthi@users.noreply.github.com>
2024-05-19 13:20:59 +0530

    Slider and search bar changes

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -8,6 +8,7 @@ import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
 import boy from "../../img/boy.png";
 import Forms from "./Forms";
+import Carousel from "../Carousel/Slider";
 
 
 const Intro = () => {
@@ -39,10 +40,13 @@ const Intro = () => {
           <img src={Github} alt="" className="transform scale-50 hover:scale-75" />
           <img src={messenger} alt="" className="transform scale-50 hover:scale-75" />
         </div>
+        <div className="max-w-2xl p-2 bg-white shadow-md rounded-md">
+<Carousel/>
+</div>
       </div>
       {/* right image side */}
       <div className="flex flex-1 relative">
-              <div className="max-w-2xl p-8 bg-white shadow-md rounded-md">
+              <div className="max-w-2xl ml-10 p-8 bg-white shadow-md rounded-md">
                 <Forms.GenderInputForm />
                 <Forms.AgeRangeForm />
                 <Forms.EventTypeInputForm />
@@ -51,7 +55,11 @@ const Intro = () => {
                 <Forms.PriceRangeInput />
               </div>
       </div>
+      
+
       </div>
+
+
 
   );
 };

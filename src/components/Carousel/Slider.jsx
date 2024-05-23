342a534aa657aed8927df7df25be44a3f02cbefb
Shlok Verma <87667355+SoraUchiha@users.noreply.github.com>
2024-05-23 18:59:02 +0530

    push

--- src/components/Carousel/Slider.jsx
+++ src/components/Carousel/Slider.jsx
@@ -1,66 +1,67 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectCoverflow, Pagination, Navigation } from "swiper";
+import "swiper/swiper-bundle.min.css";
 import img1 from "./images/img1.jpeg";
 import img2 from "./images/img2.jpeg";
 import img3 from "./images/img3.jpeg";
 import "./Slider.css";
 
 function Carousel({ onReviewClick }) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1,
-        },
-      },
-    ],
-  };
-
   return (
-    <div className="max-w-lg h-75">
+    <div className="max-w h-75 mt-10">
       <div className="mt-2 mb-16">
-        <Slider {...settings}>
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={3}
+          pagination={{ clickable: true, el: '.custom-pagination' }}
+          effect="coverflow"
+          centeredSlides={true}
+          loop={true}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            modifier: 1,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            600: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+          }}
+          modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+        >
           {data.map((d, index) => (
-            <div key={index}>
-              <div className="flex justify-center items-center w-full">
+            <SwiperSlide key={index} className="rounded-3xl">
+              <div className="flex justify-center items-center w-full mt-2">
                 <img
                   src={d.img}
                   alt=""
-                  className="items-center h-60 w-48 rounded-lg border-black shadow-xl cursor-pointer"
+                  className="items-center h-60 w-48 rounded-lg border-black cursor-pointer"
                   onClick={() => onReviewClick(d.review)}
                 />
               </div>
 
               <div
-                className="bg-white h-22 w-full font-italic text-black rounded-xl flex flex-col items-center justify-center gap-7 mt-3 mb-2 border-black shadow-lg cursor-pointer"
+                className="bg-gray-50 h-22 w-full font-italic text-black rounded-xl flex flex-col items-center justify-center gap-7 mt-3 mb-2 border-black shadow-lg cursor-pointer"
                 onClick={() => onReviewClick(d.review)}
               >
-                <p className="text-center font-italic italic text-xs">
+                <p className="text-center fuzzy-bubbles-bold text-orange-400 text-base">
                   {d.review}
                 </p>
               </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </Slider>
+        </Swiper>
+        <div className="custom-pagination" style={{ marginTop: '40px', textAlign: 'center' }}></div>
       </div>
     </div>
   );
@@ -70,27 +71,32 @@ const data = [
   {
     img: img1,
     review:
-      "Celebrate love with a heartfelt gift that speaks volumes, showing your special someone how much they mean to you.",
+      "Best Gift to Give this christmas.",
   },
   {
     img: img2,
     review:
-      "Cherish your friends with tokens of appreciation, strengthening the bond that makes your friendship unique and enduring.",
+      "Best Candy to give this Halloween.",
   },
   {
     img: img3,
     review:
-      "Commemorate your union with a meaningful gift that symbolizes the love and commitment shared in your journey together.",
+      "Best Gift for this Valentine.",
   },
   {
     img: img1,
     review:
-      "Celebrate love with a heartfelt gift that speaks volumes, showing your special someone how much they mean to you.",
+      "Best Gift to Give this christmas.",
   },
   {
     img: img2,
     review:
-      "Cherish your friends with tokens of appreciation, strengthening the bond that makes your friendship unique and enduring.",
+      "Best Candy to give this Halloween.",
+  },
+  {
+    img: img3,
+    review:
+      "Best Gift for this Valentine.",
   },
 ];
 
